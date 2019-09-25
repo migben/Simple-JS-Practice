d578c6ba54e46291fd4a50041fc6442cfef7839a
migben <miguelangelben1990@gmail.com>
2019-09-25 01:28:06 -0400

    completed the basic styles for html and downloaded imgs/sounds

---
+++ index.js
@@ -0,0 +1,11 @@
+let numBtns = document.querySelectorAll('.sound').length;
+
+for(let i = 0; i < numBtns ; i++) {
+    document.querySelectorAll('.sound')[i].addEventListener('click', function () {
+
+
+    });
+}
+
+
+
