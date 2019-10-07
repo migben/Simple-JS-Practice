09e404ed0983411eb60ef7c4f85ac55d5222c691
migben <miguelangelben1990@gmail.com>
2019-10-07 07:11:11 -0400

    finished setting up the mins, sec and hour hands

--- clock/main.js
+++ clock/main.js
@@ -1,8 +1,22 @@
+const secsHand =  document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
 const setDate = () => {
     const now = new Date();
-    const secs = now.getSeconds
+    // seconds hand
+    const secs = now.getSeconds();
+    const secDeg = (secs / 60) * 90;
+    secsHand.style.transform = `rotate(${secDeg}deg)`;
+    // minutes hand
+    const mins = now.getMinutes();
+    const minsDeg = ((mins / 60 ) * 360) + 90;
+    minsHand.style.transform =  `rotate(${minsDeg}deg)`;
+    // hours hand
+    const hour = now.getMinutes();
+    const hourDeg = ((hour / 60) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDeg}deg)`;
+
 }
 
-setInterval(() => {
-
-}, 1000);
+setInterval(setDate, 1000);
